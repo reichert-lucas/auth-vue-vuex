ea6f887f7d8759c07ce40707023725830d15af11
Lucas Reochert <lucas3d4783@gmail.com>
2021-09-09 12:01:41 -0300

    Finalização da Primeira aula - Criação do componente para a criação de usuários

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Gerentes from '../views/Gerentes.vue'
 import Home from '../views/Home.vue'
+import NovoUsuario from '../views/NovoUsuario'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/gerentes',
     name: 'gerentes',
     component: Gerentes
+  },
+  {
+    path: '/cadastre-se',
+    name: 'novo.usuario',
+    component: NovoUsuario
   }
 ]
 
---
+++ app/src/views/NovoUsuario.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <h1>Novo Usuario</h1>
+        <form @submit.prevent="enviarFormulario">
+            <div class="form.group">
+                <label for="nome">Nome</label>
+                <input type="text" class="form-control" v-model="usuario.nome">
+            </div>
+            <div class="form.group">
+                <label for="email">E-mail</label>
+                <input type="email" class="form-control" v-model="usuario.email">
+            </div>
+            <div class="form.group">
+                <label for="senha">Senha</label>
+                <input type="password" class="form-control" v-model="usuario.senha">
+            </div>
+            <button class="btn btn-primary" type="submit">Salvar</button>
+        </form>
+    </div>
+</template> 
+
+<script>
+
+import axios from 'axios'
+
+export default {
+
+  data: function() {
+    return {
+      usuario: {
+        nome: "",
+        senha: "",
+        email: ""
+      }
+    }
+  },
+  methods: {
+    enviarFormulario () {
+        axios.post('http://localhost:8000/auth/register', this.usuario)
+                .then(res => console.log(res))
+                .catch(err => console.log(err))
+    }
+  }
+}
+</script>
